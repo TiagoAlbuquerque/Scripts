afea505a8160fcbc45c2cafb192868ff11d032f6
TiagoAlbuquerque <tiago.lbqrq@gmail.com>
2016-10-17 17:26:15 -0300

    edited lock icon

--- lock.sh
+++ lock.sh
@@ -1,7 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-scrot /tmp/screen.png
-convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
-[[ -f $1 ]] && convert /tmp/screen.png $1 -gravity center -composite -matte /tmp/screen.png
-i3lock -u -i /tmp/screen.png
-rm /tmp/screen.png
+icon=$HOME/Pictures/Icons/lock.png
+tmpbg=/tmp/screen.png
+
+(( $# )) && { icon=$1; }
+
+scrot "$tmpbg"
+convert "$tmpbg" -scale 10% -scale 1000% "$tmpbg"
+convert "$tmpbg" $icon -gravity center -composite -matte "$tmpbg"
+i3lock -i "$tmpbg"
+rm "$tmpbg"
