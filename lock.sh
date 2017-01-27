d52b422bc5f2f28926e242411edeca48a2ede9e1
Tiago Oliveira de Albuquerque <tiago.lbqrq@gmail.com>
2017-01-27 09:30:09 -0300

    ajustei o posicionamento da imagem da trava"

--- lock.sh
+++ lock.sh
@@ -6,7 +6,7 @@ tmpbg=/tmp/screen.png
 (( $# )) && { icon=$1; }
 
 scrot "$tmpbg"
-convert "$tmpbg" -scale 10% -scale 1000% "$tmpbg"
-convert "$tmpbg" $icon -gravity center -composite -matte "$tmpbg"
+convert "$tmpbg" -scale 5% -scale 2000% "$tmpbg"
+convert "$tmpbg" $icon -gravity center -geometry +0+72 -composite -matte "$tmpbg"
 i3lock -i "$tmpbg"
 rm "$tmpbg"
